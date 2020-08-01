917aa15ad94a684556a1ec85eb1c21cb67ac309c
Peter Hanley <hanley.peter@gmail.com>
2020-08-01 18:13:08 +1000

    Added badge color switching functionality for date and status

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Create the task list object array
 let taskList = [];
 let idCounter = 1;
+let dateobj = new Date();
+let date = dateobj.toDateString();
 
 // Make a function to create a new task object
 function createTask(taskName, taskDetails, taskAssignee, taskDate, taskStatus) {
@@ -21,7 +23,7 @@ let task1 = createTask(
   "Go to bank",
   "Apply for new credit card",
   "Peter",
-  "2020-08-01",
+  "2020-07-01",
   "Not started"
 );
 
@@ -91,8 +93,30 @@ function buildTaskTable() {
     col3.setAttribute("scope", "col");
 
     var col3span = document.createElement("span");
-    col3span.classList.add("badge", "badge-secondary");
+    col3span.classList.add("badge");
     col3span.innerHTML = taskList[i].duedate;
+
+    let taskDate = new Date(taskList[i].duedate);
+
+    if (
+      taskDate.getFullYear() == dateobj.getFullYear() &&
+      taskDate.getMonth() == dateobj.getMonth() &&
+      taskDate.getUTCDate() == dateobj.getUTCDate()
+    ) {
+      // task due today
+      col3span.classList.add("badge-warning");
+    } else if (taskDate.getTime() < dateobj.getTime()) {
+      // overdue at least 1 day
+      col3span.classList.add("badge-danger");
+
+      if (taskList[i].status !== ("In progress" || "Completed")) {
+        // task is not completed or in progress, switch status to overdue
+        taskList[i].status = "Overdue";
+      }
+    } else {
+      col3span.classList.add("badge-secondary");
+    }
+
     col3.appendChild(col3span);
 
     // add to the row
@@ -103,8 +127,19 @@ function buildTaskTable() {
     col4.setAttribute("scope", "col");
 
     var col4span = document.createElement("span");
-    col4span.classList.add("badge", "badge-secondary");
+    col4span.classList.add("badge");
     col4span.innerHTML = taskList[i].status;
+
+    if (taskList[i].status == "In progress") {
+      col4span.classList.add("badge-warning");
+    } else if (taskList[i].status == "Completed") {
+      col4span.classList.add("badge-success");
+    } else if (taskList[i].status == "Overdue") {
+      col4span.classList.add("badge-danger");
+    } else {
+      col4span.classList.add("badge-secondary");
+    }
+
     col4.appendChild(col4span);
 
     // add to the row
@@ -178,8 +213,6 @@ let modalDateInput = document.getElementById("dueDateInput");
 let modalStatusInput = document.getElementById("statusSelect");
 
 // set the default value of the modal date input to today's date
-var dateobj = new Date();
-var date = dateobj.toDateString();
 modalDateInput.value.innerHTML = date;
 
 function checkIfValidName(event) {
