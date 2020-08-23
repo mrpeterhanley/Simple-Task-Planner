83290fd16618f6037d9000834a7e6ced7146e9dd
catgonza <67137308+catgonza@users.noreply.github.com>
2020-08-23 21:27:28 +1000

    Testing that tasks are correctly added to DOM, all passed

--- task-manager.test.js
+++ task-manager.test.js
@@ -11,12 +11,13 @@ beforeEach(() => {
   document.documentElement.innerHTML = html.toString();
   localStorage.clear();
   const formManager = {};
-  parent = document.querySelector("#personal-tasks > .tasks");
-  const taskFilter = document.querySelector("#personal-tasks .task-filter");
-  taskManager = new TaskManager(parent, formManager, taskFilter);
+  parent = document.querySelector("#taskTableBody");
+  taskManager = new TaskManager("personaltaskManager");
 });
 
 test("Tasks should be correctly added to DOM", () => {
+  expect(taskManager).toBeDefined();
+  expect(parent).toBeDefined();
   expect(taskManager.tasks.length).toBe(0);
   expect(parent.children.length).toBe(0);
 
@@ -28,8 +29,17 @@ test("Tasks should be correctly added to DOM", () => {
     "In Progress"
   );
 
+  taskManager.refreshTaskTable();
+
   expect(taskManager.tasks.length).toBe(1);
-  expect(parent.children.length).toBe(1);
+  expect(parent.children.length).toBe(2);
+
+  expect(taskManager.tasks[0].id).toBe(1);
+  expect(taskManager.tasks[0].name).toBe("task Name 1");
+  expect(taskManager.tasks[0].details).toBe("task Details 1");
+  expect(taskManager.tasks[0].assignee).toBe("task Assignee 1");
+  expect(taskManager.tasks[0].duedate).toBe("2020-08-04");
+  expect(taskManager.tasks[0].status).toBe("In Progress");
 
   taskManager.addTask(
     "task Name 2",
@@ -38,7 +48,14 @@ test("Tasks should be correctly added to DOM", () => {
     "2020-09-27",
     "Not started"
   );
-
+  taskManager.refreshTaskTable();
   expect(taskManager.tasks.length).toBe(2);
-  expect(parent.children.length).toBe(2);
+  expect(parent.children.length).toBe(4);
+
+  expect(taskManager.tasks[1].id).toBe(2);
+  expect(taskManager.tasks[1].name).toBe("task Name 2");
+  expect(taskManager.tasks[1].details).toBe("task Details 2");
+  expect(taskManager.tasks[1].assignee).toBe("task Assignee 2");
+  expect(taskManager.tasks[1].duedate).toBe("2020-09-27");
+  expect(taskManager.tasks[1].status).toBe("Not started");
 });
